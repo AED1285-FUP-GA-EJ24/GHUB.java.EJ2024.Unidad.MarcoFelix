a91d6627a3996eee7114dc4d2eb43f34a5cafe89
Marco Antonio Felix Torrez <159277886+MarcoFelix13@users.noreply.github.com>
2024-05-21 14:36:04 +0000

    Demostracion de Arreglos

---
+++ demoArreglos/DemoArreglos.java
@@ -0,0 +1,11 @@
+package demoArreglos;
+public class DemoArreglos{
+    public void demoArreglos(){
+        System.out.println("Demostración de arreglos unidimensionales");
+        //declarando un arreglo de tamaño 10;
+        int c[] = new int[10];
+
+
+
+    }
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,10 +1,45 @@
 package miPrincipal;
-
-
+import demoArreglos.DemoArreglos;
+import java.util.Scanner;
 
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        do{
+            System.out.println("**************************************");
+            System.out.println("      MENU DE OPCIONES UNIDAD IV      ");
+            System.out.println("**************************************");
+            System.out.println("* 1) Demo de arreglos unidimensionales"); 
+            System.out.println("* 2) "); 
+            System.out.println("* 3) "); 
+            System.out.println("* 4) "); 
+            System.out.println("* 5) "); 
+            System.out.println();
+            System.out.println("* 99) Salir"); 
+            System.out.println("**************************************");
+            System.out.print("Seleccion Opcion:");
+            opcion = entrada.nextInt();
+            switch(opcion){
+                case 1:
+                   //creo el objeto
+                   DemoArreglos objDemoArreglos = new DemoArreglos();
+                   objDemoArreglos.demoArreglos();
+                   break;
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+
+                case 99:
+                    System.out.println("Hasta luego");
+                    break;
+                default:
+                 System.out.println("Opción inválida");
+            }
+
+        }while(opcion != 99);
+        entrada.close();
     }
 }
